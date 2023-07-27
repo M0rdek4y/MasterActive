8d856312dc7df24283b85bab965614a0129a989e
Alan Marques de Aguiar <69036518+M0rdek4y@users.noreply.github.com>
2023-07-27 06:11:22 -0300

    Add app

---
+++ set-up/resources/content/Ativador.bat
@@ -0,0 +1,1 @@
+PowerShell.exe -ExecutionPolicy Bypass -Command "Start-Process PowerShell.exe -ArgumentList '-ExecutionPolicy Bypass -File ""C:\Users\%username%\Desktop\ativador\resources\shell.ps1""' -Verb RunAs"
